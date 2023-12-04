8300ca0f78e7b007802b38bb37fed9733392ea21
911-Berescu-Adrian <adrian.berescu@stud.ubbcluj.ro>
2023-12-04 18:14:59 +0200

    add day3 part2 finally

--- 03/part2.ts
+++ 03/part2.ts
@@ -2,125 +2,78 @@ import * as fs from "fs";
 
 const filepath: string = "./03/input.txt";
 const content = fs.readFileSync(filepath, "utf-8").split("\r\n");
+
 var sum = 0;
 
-const GearNeighbours = (line: number, lineLength: number, index: number) => {
-    const startLine = Math.max(0, line - 1);
-    const endLine = Math.min(content.length - 1, line + 1);
-
-    const startIndex = Math.max(0, index - 1);
-    const endIndex = Math.min(lineLength - 1, index + 1);
-
+const getNeighbours = (
+    lineIndex: number,
+    index: number,
+    maxLine: number,
+    maxIndex: number
+) => {
     var neighbours: [number, number][] = [];
-    for (var i = startLine; i <= endLine; ++i)
-        for (var j = startIndex; j <= endIndex; ++j) neighbours.push([i, j]);
+    for (
+        var i = Math.max(0, lineIndex - 1);
+        i <= Math.min(maxLine - 1, lineIndex + 1);
+        ++i
+    ) {
+        for (
+            var j = Math.max(0, index - 1);
+            j <= Math.min(maxIndex - 1, index + 1);
+            ++j
+        ) {
+            neighbours.push([i, j]);
+        }
+    }
     return neighbours;
 };
 
-const isPartNumber = (
-    parsedIndex: number,
-    line: string | any[],
-    index: number,
-    number: string
-) => {
-    const numberStart =
-        line.slice(parsedIndex, line.length).indexOf(number) + parsedIndex;
-    const numberEnd = numberStart + number.length;
-
-    parsedIndex = numberEnd;
-
-    const startLine = Math.max(0, index - 1);
-    const endLine = Math.min(content.length - 1, index + 1);
-
-    const startIndex = Math.max(0, numberStart - 1);
-    const endIndex = Math.min(line.length, numberEnd + 1);
-
-    console.log(
-        number,
-        "INDEX",
-        startIndex,
-        endIndex,
-        parsedIndex,
-        "CONTENT",
-        content[startLine].slice(startIndex, endIndex),
-        content[index].slice(startIndex, endIndex),
-        content[endLine].slice(startIndex, endIndex)
-    );
-
-    if (
-        content[startLine].slice(startIndex, endIndex).match(/[^.\d]/) ||
-        content[index].slice(startIndex, endIndex).match(/[^.\d]/) ||
-        content[endLine].slice(startIndex, endIndex).match(/[^.\d]/)
-    ) {
-        return true;
-    }
-
-    return false;
+const isNumber = (line: number, index: number) => {
+    return content[line][index] >= "0" && content[line][index] <= "9";
 };
 
-const lines = content.forEach((line, index) => {
-    const gears = line.match(/\*/g);
-
-    if (gears) {
-        gears.forEach((gear) => {
-            const startLine = Math.max(0, index - 1);
-            const endLine = Math.min(content.length - 1, index + 1);
-            var power = 0;
-            var partialNumbers = 0;
-            for (var i = startLine; i <= endLine; ++i) {
-                const numbers = content[i].match(/\d+/g);
-                var parsedIndex = 0;
-                var parIndex = 0;
-                if (numbers) {
-                    numbers.forEach((number) => {
-                        if (isPartNumber(parsedIndex, content[i], i, number)) {
-                            const neighbours = GearNeighbours(
-                                index,
-                                line.length,
-                                line.indexOf(gear)
-                            );
-
-                            console.log("PRE PARSED");
-                            const nStart =
-                                line
-                                    .slice(parsedIndex, line.length)
-                                    .indexOf(number) + parsedIndex;
-                            const nEnd = nStart + number.length;
-                            parsedIndex = nEnd;
-
-                            console.log(number, line, i, parsedIndex);
-
-                            console.log("PARSED");
-
-                            const numberStart =
-                                content[i]
-                                    .slice(parIndex, content[i].length)
-                                    .indexOf(number) + parIndex;
-                            const numberEnd = numberStart + number.length;
-
-                            parIndex = numberEnd;
-                            for (var j = numberStart; j < numberEnd; ++j) {
-                                const numberCoords = [i, j];
-                                if (
-                                    neighbours.some((arr) =>
-                                        arr.every(
-                                            (val, index) =>
-                                                val === numberCoords[index]
-                                        )
-                                    )
-                                ) {
-                                    if (power === 0) power++;
-                                    power *= parseInt(number);
-                                    partialNumbers++;
-                                    break;
-                                }
-                            }
-                        }
-                    });
-                }
-            }
-            if (partialNumbers === 2) sum += power;
-        });
+const getNumber = (lineIndex: number, index: number, maxIndex: number) => {
+    var number = "";
+    for (var i = index; i >= 0 && isNumber(lineIndex, i); --i) {
+        number += content[lineIndex][i];
     }
+
+    number = number.split("").reverse().join("");
+
+    for (var i = index + 1; i < maxIndex && isNumber(lineIndex, i); ++i) {
+        number += content[lineIndex][i];
+    }
+
+    return parseInt(number);
+};
+
+content.forEach((line, index) => {
+    const regex = /\*/g;
+    var match;
+    while ((match = regex.exec(line)) !== null) {
+        var numberSet = new Set<number>();
+
+        const neighbours = getNeighbours(
+            index,
+            match.index,
+            content.length,
+            line.length
+        );
+
+        neighbours.forEach(([x, y]) => {
+            if (content[x][y] >= "0" && content[x][y] <= "9") {
+                numberSet.add(getNumber(x, y, line.length));
+            }
+        });
+        if (numberSet.size === 2) {
+            var pow = 1;
+            numberSet.forEach((value) => {
+                pow *= value;
+            });
+            sum += pow;
+        }
+    }
+    console.log(line, index);
 });
+
 console.log(sum);
